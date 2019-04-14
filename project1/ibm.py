1966224fac23131df5c33c96f3f3705a1c20d9f3
mario <m.giulianelli.m@gmail.com>
2019-04-14 18:06:03 +0200

    Compute convergence statistics

--- project1/ibm.py
+++ project1/ibm.py
@@ -1,7 +1,9 @@
 from collections import defaultdict
 from tqdm import tqdm
+import numpy as np
 
 from .data_reader import DataReader
+from .aer import read_naacl_alignments, AERSufficientStatistics
 
 NULL_TOKEN = '[NULL]'
 EPSILON = 1e-12
@@ -22,7 +24,7 @@ class IBM1(object):
         Runs the Expectation Maximization algorithm to estimate translation probabilities.
     """
 
-    def __init__(self, source_path, target_path):
+    def __init__(self, source_path, target_path, source_path_valid, target_path_valid, gold_path_valid):
         """
         :param source_path: str
             The path to the source corpus. Whitespace separates tokens, newline separates sequences (sentences).
@@ -30,7 +32,10 @@ class IBM1(object):
             The path to the target corpus. Whitespace separates tokens, newline separates sequences (sentences).
             The target corpus should have the same length of the source corpus.
         """
-        self.data_reader = DataReader(source_path, target_path)
+        self.training_data = DataReader(source_path, target_path)
+        self.validation_data = DataReader(source_path_valid, target_path_valid)
+        self.gold_links = read_naacl_alignments(gold_path_valid)
+
         self.translation_probs = defaultdict(lambda: EPSILON)
 
 
@@ -42,12 +47,17 @@ class IBM1(object):
             The number of EM iterations over the parallel corpus.
         :return: None
         """
-        for _ in range(n_iterations):
+        print('Start training...')
+        for iter in range(n_iterations):
 
+            print('Epoch {}.'.format(iter), end='\t')
+
+            # EXPECTATION MAXIMISATION
             translation_counts = defaultdict(float)  # How often do 'e' and 'f' form a translation pair?
             occurrence_counts = defaultdict(float)   # How often does 'e' appear in the corpus?
+            training_log_likelihood = 0
 
-            for (source, target) in tqdm(self.data_reader.get_parallel_data(), total=len(self.data_reader)):
+            for (source, target) in tqdm(self.training_data.get_parallel_data(), total=len(self.training_data)):
 
                 # Add NULL token to naively cope with fertility
                 source = [NULL_TOKEN] + source
@@ -67,8 +77,32 @@ class IBM1(object):
                         translation_counts[(s, t)] += delta
                         occurrence_counts[s] += delta
 
+                    training_log_likelihood += np.log(np.max([self.translation_probs[(t, s)] for s in source]))
+
+            print('Training log-likelihood: {}'.format(training_log_likelihood), end='\t')
+
             for (s,t) in translation_counts:
                 self.translation_probs[(t, s)] = translation_counts[(s, t)] / occurrence_counts[s]
+
+
+            # DECODING
+            print('Validation... ')
+            metric = AERSufficientStatistics()
+            predictions = []
+
+            for (source, target) in tqdm(self.validation_data.get_parallel_data(), total=len(self.validation_data)):
+                links = set()
+                for i, t in enumerate(target, start=1):
+                    link = (np.argmax([self.translation_probs[(t,s)] for s in source]), i)
+                    links.add(link)
+                predictions.append(links)
+
+            for gold, pred in zip(self.gold_links, predictions):
+                metric.update(sure=gold[0], probable=gold[1], predicted=pred)
+
+            # AER
+            print('Epoch {}. AER: {}'.format(iter, metric.aer()))
+
 
 class IBM2(object):
     """
@@ -89,7 +123,7 @@ class IBM2(object):
         Runs the Expectation Maximization algorithm to estimate translation and alignment probabilities.
     """
 
-    def __init__(self, source_path, target_path, alignment='positional'):
+    def __init__(self, source_path, target_path, source_path_valid, target_path_valid, gold_path_valid, alignment='positional'):
         """
         :param source_path: str
             The path to the source corpus. Whitespace separates tokens, newline separates sequences (sentences).
@@ -99,14 +133,17 @@ class IBM2(object):
         :param alignment: str
             Positional alignment ('positional') or Vogel et al.'s jump distribution ('jump').
         """
-        self.data_reader = DataReader(source_path, target_path)
-        self.translation_probs = defaultdict(lambda: EPSILON)
-
         if alignment not in ['positional', 'jump']:
             raise ValueError("Alignment can be 'positional' or according to a 'jump' distribution")
-
         self.alignment = alignment
+
         self.alignment_probs = defaultdict(lambda: EPSILON)
+        self.translation_probs = defaultdict(lambda: EPSILON)
+
+        self.training_data = DataReader(source_path, target_path)
+        self.validation_data = DataReader(source_path_valid, target_path_valid)
+
+        self.gold_links = read_naacl_alignments(gold_path_valid)
 
 
     def train(self, n_iterations):
@@ -124,10 +161,15 @@ class IBM2(object):
             The number of EM iterations over the parallel corpus.
         :return: None
         """
-        for _ in range(n_iterations):
+        print('Start training...')
+        for iter in range(n_iterations):
 
+            print('Epoch {}.'.format(iter), end='\t')
+
+            # EXPECTATION MAXIMISATION
             translation_counts = defaultdict(float)  # How often do 'e' and 'f' form a translation pair?
             occurrence_counts = defaultdict(float)   # How often does 'e' appear in the corpus?
+            training_log_likelihood = 0
 
             # How often does the ith target word align with the jth source word...
             # given a target and a source sentence of length m and l respectively?
@@ -136,7 +178,7 @@ class IBM2(object):
             # How often does an l-long source sentence align with an m-long target sentence?
             length_alignment_counts = defaultdict(float)
 
-            for (source, target) in tqdm(self.data_reader.get_parallel_data(), total=len(self.data_reader)):
+            for (source, target) in tqdm(self.training_data.get_parallel_data(), total=len(self.training_data)):
 
                 # Add NULL token to naively cope with fertility
                 source = [NULL_TOKEN] + source
@@ -144,16 +186,16 @@ class IBM2(object):
                 l = len(source)
                 m = len(target)
 
-                for i, t in enumerate(target):
+                for i, t in enumerate(target, start=1):
 
                     # First compute normalisation constant for this target word 'f'.
                     # It is necessary to update all counts involving 'f'.
                     delta_denominator = 0
-                    for (j, s) in enumerate(source):
+                    for (j, s) in enumerate(source, start=1):
                         delta_denominator += self.alignment_probs[(j, i, l, m)] * self.translation_probs[(t, s)]
 
                     # Now update the counts.
-                    for (j, s) in enumerate(source):
+                    for (j, s) in enumerate(source, start=1):
                         delta = self.translation_probs[(t, s)] / delta_denominator
 
                         translation_counts[(s, t)] += delta
@@ -162,9 +204,47 @@ class IBM2(object):
                         positional_alignment_counts[(j, i, l, m)] += delta
                         length_alignment_counts[(i, l, m)] += delta
 
+                    training_log_likelihood += \
+                        np.log(
+                            np.max(
+                                [self.translation_probs[(t, s)] * self.alignment_probs[(j, i, l, m)]
+                                 for (j, s)
+                                 in enumerate(source, start=1)])
+                        )
+
+            print('Training log-likelihood: {}'.format(training_log_likelihood), end='\t')
+
             for (s,t) in translation_counts:
                 self.translation_probs[(t, s)] = translation_counts[(s, t)] / occurrence_counts[s]
 
             for (j, i, l, m) in positional_alignment_counts:
                 self.alignment_probs[(j, i, l, m)] = \
                     positional_alignment_counts[(j, i, l, m)] / length_alignment_counts[(i, l, m)]
+
+            # DECODING
+            print('Validation...')
+            metric = AERSufficientStatistics()
+            predictions = []
+
+            for (source, target) in tqdm(self.validation_data.get_parallel_data(), total=len(self.validation_data)):
+                l = len(source)
+                m = len(target)
+                links = set()
+                for i, t in enumerate(target, start=1):
+                    link = (
+                        np.argmax(
+                            [self.translation_probs[(t, s)] * self.alignment_probs[(j, i, l, m)]
+                             for (j, s)
+                             in enumerate(source, start=1)]),
+                        i
+                    )
+                    links.add(link)
+                predictions.append(links)
+
+            for gold, pred in zip(self.gold_links, predictions):
+                metric.update(sure=gold[0], probable=gold[1], predicted=pred)
+
+            # AER
+            print('Epoch {}. AER: {}'.format(iter, metric.aer()))
+
+
