d3d752783b27a68407ba93901aaf4b823d414457
mario <m.giulianelli.m@gmail.com>
2019-04-17 10:15:30 +0200

    get ready for validation

--- project1/ibm1.py
+++ project1/ibm1.py
@@ -3,25 +3,28 @@ from collections import defaultdict
 from tqdm import tqdm
 
 from .data_reader import DataReader
+from .aer import read_naacl_alignments, AERSufficientStatistics
 
 NULL_TOKEN = 'NULL'
 
 
 class IBM1:
-    def __init__(self, source_path: str, target_path) -> None:
-        self.data_reader = DataReader(source_path, target_path)
+    def __init__(self, source_path_train: str, target_path_train, source_path_valid, target_path_valid) -> None:
+        self.train_data_reader = DataReader(source_path_train, target_path_train)
+        self.valid_data_reader = DataReader(source_path_valid, target_path_valid)
 
-        init_ef_norm = 1 / (self.data_reader.n_source_types * self.data_reader.n_target_types)
+        init_ef_norm = 1 / (self.train_data_reader.n_source_types * self.train_data_reader.n_target_types)
         self.probs_ef: Dict[Tuple[str, str], float] = defaultdict(lambda: init_ef_norm)
 
     def train(self, n_iter: int):
         for s in range(n_iter):
             counts_ef = defaultdict(float)
             counts_e = defaultdict(float)
+            training_log_likelihood = 0
 
             # Maximization
-            for k in tqdm(range(len(self.data_reader))):
-                e, f = self.data_reader[k]
+            for k in tqdm(range(len(self.train_data_reader))):
+                e, f = self.train_data_reader[k]
                 e = [NULL_TOKEN] + e
 
                 e_normalizer = defaultdict(float)
@@ -35,6 +38,8 @@ class IBM1:
 
                         counts_ef[we, wf] += delta
                         counts_e[wf] += delta
+
+            # training_log_likelihood +=
 
             # Expectation
             for (we, wf), c in counts_ef.items():
