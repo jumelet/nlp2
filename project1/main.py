0911678e3f0687f589c2ab0f211e3b691d1a3ba0
jumelet <jumeletjaap@gmail.com>
2019-04-17 16:11:32 +0200

    Refactor ibm classes into 1 class

--- project1/main.py
+++ project1/main.py
@@ -1,10 +1,20 @@
-from project1.ibm1 import IBM1
+from project1.ibm import IBM
 
-ibm1 = IBM1('./data/training/hansards.36.2.e',
-            './data/training/hansards.36.2.f',
-            './data/validation/dev.e',
-            './data/validation/dev.f',
-            './data/validation/dev.wa.nonullalign')
+ibm1 = IBM('IBM1',
+           './data/training/hansards.36.2.e',
+           './data/training/hansards.36.2.f',
+           './data/validation/dev.e',
+           './data/validation/dev.f',
+           './data/validation/dev.wa.nonullalign')
 
-ibm1.train(1)
+
+ibm2 = IBM('IBM2',
+           './data/training/hansards.36.2.e',
+           './data/training/hansards.36.2.f',
+           './data/validation/dev.e',
+           './data/validation/dev.f',
+           './data/validation/dev.wa.nonullalign')
+
+ibm1.train(3)
+# ibm2.train(3)
 
