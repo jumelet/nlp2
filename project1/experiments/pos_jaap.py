174306f09778a75f74a6e9a92a8865cf8e8665af
jumelet <jumeletjaap@gmail.com>
2019-04-26 15:42:09 +0200

    add output files

--- project1/experiments/pos_jaap.py
+++ project1/experiments/pos_jaap.py
@@ -3,6 +3,7 @@ import pickle
 import sys
 from collections import Counter, defaultdict
 from pprint import pprint
+from project1.aer import read_naacl_alignments, AERSufficientStatistics
 
 import numpy as np
 
@@ -30,18 +31,18 @@ def read_alignments(alignment_path: str):
     sure and possible alignemts. Dictionaries map a sentence key to a list of
     (a_s, a_t) links.
     """
-    align = defaultdict(list)
-    align_s = defaultdict(list)
-    align_p = defaultdict(list)
+    align = defaultdict(set)
+    align_s = defaultdict(set)
+    align_p = defaultdict(set)
     with open(alignment_path) as f:
         lines = [l.strip().split(' ') for l in f.read().strip().split('\n')]
         for l in lines:
-            align[int(l[0])].append((int(l[1]), int(l[2])))
+            align[int(l[0])].add((int(l[1]), int(l[2])))
             if l[3] == 'S':
-                align_s[int(l[0])].append((int(l[1]), int(l[2])))
-                align_p[int(l[0])].append((int(l[1]), int(l[2])))  # S also in P!
+                align_s[int(l[0])].add((int(l[1]), int(l[2])))
+                align_p[int(l[0])].add((int(l[1]), int(l[2])))  # S also in P!
             else:
-                align_p[int(l[0])].append((int(l[1]), int(l[2])))
+                align_p[int(l[0])].add((int(l[1]), int(l[2])))
 
     return align, align_s, align_p
 
@@ -97,6 +98,8 @@ def calc_pos_alignments(en, fr, align_s, align_p, enpos, frpos):
             elif a in a_p:
                 pos_p[a_pos] += 1
             else:
+                if a_pos == ('ADP', 'DET'):
+                    print(e[a_e], f[a_f - 1])
                 pos_wrong[a_pos] += 1
 
     return pos_s, pos_p, pos_wrong
@@ -114,7 +117,6 @@ def calc_multiple_accuracy(en, fr, align_p):
     cor_mult = 0
 
     defaultprob = PROBS['bla', 'bla']
-    print(defaultprob)
 
     # The boy    and the dog
     #  |
@@ -132,9 +134,13 @@ def calc_multiple_accuracy(en, fr, align_p):
 
         for i, wt in enumerate(t, start=1):
             maxlink = np.argmax(
-                [calc_ef_prob(wt, ws, j, i, len_s, len_t) for j, ws in enumerate(s)]
+                [
+                    (calc_ef_prob(wt, ws, j, i, len_s, len_t))  # - int(j in maxlinks and (ws in s[:j] or ws in s[j+1:]))*1e-12)
+                    for j, ws in enumerate(s)
+                ]
             )
-            if maxlink != 0 and ((s[maxlink] in s[maxlink+1:] or s[maxlink] in s[:maxlink])): # and maxlink not in maxlinks):
+
+            if maxlink != 0 and ((s[maxlink] in s[maxlink+1:] or s[maxlink] in s[:maxlink]) and maxlink in maxlinks):
                 mult_a.add((maxlink, i))
             elif maxlink != 0:
                 a.add((maxlink, i))
@@ -217,6 +223,43 @@ def calc_pos_sequence_alignments(seq_len, en, fr, align_s, align_p, enpos, frpos
     return en_seq_s, en_seq_p, en_seq_wrong, fr_seq_s, fr_seq_p, fr_seq_wrong
 
 
+def create_test_file():
+    def get_alignments(s, t):
+        a = set()
+        maxlinks = []
+        len_s = len(s)
+        len_t = len(t)
+
+        for i, wt in enumerate(t, start=1):
+            maxlink = np.argmax(
+                [
+                    (calc_ef_prob(wt, ws, j, i, len_s, len_t)) # - int(j in maxlinks and (ws in s[:j] or ws in s[j+1:]))*1e-12)
+                    for j, ws in enumerate(s)
+                ]
+            )
+
+            if maxlink != 0:
+                a.add((maxlink, i))
+            maxlinks.append(maxlink)
+        return a
+
+    en_pos, en_sen = read_pos('../data/tagged/test.pos.e', '../data/testing/test/test.e')
+    fr_pos, fr_sen = read_pos('../data/tagged/test.pos.f', '../data/testing/test/test.f')
+
+    metric = AERSufficientStatistics()
+
+    gold_links = read_naacl_alignments('../data/testing/answers/test.wa.nonullalign')
+
+    with open('../data/testing/eval/test_align.txt', 'w+') as f:
+        for i, (s, t) in zip(en_sen.keys(), zip(en_sen.values(), fr_sen.values())):
+            aligns = get_alignments(['NULL']+s, t)
+            metric.update(sure=sure_a[i], probable=prob_a[i], predicted=aligns)
+            for align in aligns:
+                f.write(f'{"0"*(4-len(str(i)))}{i} {align[0]} {align[1]}\n')
+
+    print(metric.aer())
+
+
 if __name__ == '__main__':
     # MODEL PROBS READING #
     IBM_TYPE = 'IBM2'
@@ -228,14 +271,16 @@ if __name__ == '__main__':
             ALIGN_PROBS = pickle.load(file)
 
     experiments = {
-        'pos_alignments': True,
+        'pos_alignments': False,
         'pos_sequence_alignments': False,
         'multiple_tokens':  False,
     }
-    en_pos, en_sen = read_pos('../data/tagged/dev.pos.e', '../data/testing/test/test.e')
-    fr_pos, fr_sen = read_pos('../data/tagged/dev.pos.f', '../data/testing/test/test.f')
+    en_pos, en_sen = read_pos('../data/tagged/test.pos.e', '../data/testing/test/test.e')
+    fr_pos, fr_sen = read_pos('../data/tagged/test.pos.f', '../data/testing/test/test.f')
 
     all_a, sure_a, prob_a = read_alignments('../data/testing/answers/test.wa.nonullalign')
+
+    create_test_file()
 
     if experiments['pos_alignments']:
         sure_pos_a, prob_pos_a, wrong_pos_a = calc_pos_alignments(en_sen, fr_sen,
@@ -272,13 +317,16 @@ if __name__ == '__main__':
 
         mc = None
         print('most common pos2pos in sure links:')
-        pprint(sure_pos_diff.most_common(mc))
+        pprint([(k, v, sure_pos_a.get(k, 0), sure_pos_a1.get(k, 0))
+                for k, v in sure_pos_diff.most_common(mc)])
 
         print('\nmost common pos2pos in probable-sure links:')
-        pprint(prob_pos_diff.most_common(mc))
+        pprint([(k, v, prob_pos_a.get(k, 0), prob_pos_a1.get(k, 0))
+                for k, v in prob_pos_diff.most_common(mc)])
 
         print('\nmost common wrong pos2pos links')
-        pprint(wrong_pos_diff.most_common(mc))
+        pprint([(k, v, wrong_pos_a.get(k, 0), wrong_pos_a1.get(k, 0))
+                for k, v in wrong_pos_diff.most_common(mc)])
 
     if experiments['pos_sequence_alignments']:
         en_seq_s, en_seq_p, en_seq_wrong, fr_seq_s, fr_seq_p, fr_seq_wrong = calc_pos_sequence_alignments(
