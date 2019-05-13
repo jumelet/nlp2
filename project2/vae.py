abfeb8c94023ad59ee34999dffb3f9834ef91e8f
mario <m.giulianelli.m@gmail.com>
2019-05-13 19:27:50 +0200

    fix the hell out of this VAE

--- project2/vae.py
+++ project2/vae.py
@@ -12,9 +12,9 @@ import torch
 import torch.utils.data
 from torch import nn, optim
 from torch.nn import functional as F
-from torchvision import datasets, transforms
 from nltk.tree import Tree
 from collections import Counter
+from torch.nn.utils.rnn import pad_sequence
 
 
 parser = argparse.ArgumentParser(description='VAE MNIST Example')
@@ -38,54 +38,67 @@ device = torch.device("cuda" if args.cuda else "cpu")
 kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
 
 EOS = '[EOS]'
+BOS = '[BOS]'
+UNK = '[UNK]'
+
+
+def batches(l, n):
+    for i in range(0, len(l), n):
+        yield l[i:i+n]
 
 
 class Split(object):
-    def __init__(self, sentences, n=None):
+    def __init__(self, sentences, w2i, n=None):
         self.sentences = sentences
+        self.w2i = w2i
         if n is None:
             self.len = len(sentences)
         else:
             self.len = n
 
-    def data(self):
-        for s in self.sentences:
-            yield s
+    def data(self, batch_size):
+        for batch in batches(self.sentences, batch_size):
+            batch = [torch.tensor([self.w2i[token] for token in sentence]) for sentence in batch]
+            yield pad_sequence(batch)
 
     def __len__(self):
         return self.len
 
 
-class CorpusLoader(object):
-    def __init__(self, train_path, test_path, validation_path=None):
-        counter = Counter([EOS])
+class Corpus(object):
+    def __init__(self, train_path, validation_path, test_path):
+        counter = Counter()
+        train_sentences, train_len, counter = self.load(train_path, counter)
+        valid_sentences, valid_len, counter = self.load(validation_path, counter)
+        test_sentences, test_len, counter = self.load(test_path, counter)
 
-        sentences, len, counter = self.load(train_path, counter)
-        self.training = Split(sentences, len)
-
-        sentences, len, counter = self.load(train_path, counter)
-        self.test = Split(sentences, len)
-
-        if validation_path:
-            sentences, len, counter = self.load(validation_path, counter)
-            self.validation = Split(sentences, len)
-
-        self.vocab = [w for (w, freq) in counter.most_common()]
+        self.vocab = [UNK, EOS, BOS] + [w for (w, freq) in counter.most_common()]
         self.w2i = {w: i for (i, w) in enumerate(self.vocab)}
         self.i2w = {i: w for (w, i) in self.w2i.items()}
+
+        self.training = Split(train_sentences, self.w2i, len)
+        self.test = Split(test_sentences, self.w2i, len)
+        self.validation = Split(valid_sentences, self.w2i, len)
 
     def load(self, path, counter=None):
         if counter is None:
             counter = Counter()
 
+        print(f'Reading corpus: {path}')
         sentences, len = [], 0
         with open(path, 'r') as f:
             for line in f.readlines():
                 tree = Tree.fromstring(line)
                 sentence = tree.leaves()
                 counter += Counter(sentence)
-                sentences.append(sentence + [EOS])
+                sentence = sentence + [EOS]
+                sentences.append(sentence)
+                # if len(sentence) > max_seq_len:
+                #     max_seq_len = len(sentence)
                 len += 1
+        print(f'Done. {len} sentences.')
+        # print(f'Most frequent words: {counter.most_common(10)}\n')
+
         return sentences, len, counter
 
     def tokens_to_ids(self, seq):
@@ -96,48 +109,66 @@ class CorpusLoader(object):
 
 
 class RNNEncoder(nn.Module):
-    def __init__(self, rnn_type, nlayers, vocab, edim, hdim, zdim, bidirectional=True):
+    def __init__(self, batch_size, rnn_type, nlayers, bidirectional, edim, hdim, zdim, vocab):
         super(RNNEncoder, self).__init__()
         self.w2i = {w: i for (i, w) in enumerate(vocab)}
         self.V = len(self.w2i)
 
         if rnn_type in ['LSTM', 'GRU']:
-            self.rnn = getattr(nn, rnn_type)(self.V, hdim, nlayers, bidirectional=bidirectional)
+            self.rnn = getattr(nn, rnn_type)(edim, hdim, nlayers, bidirectional=bidirectional)
         else:
             raise ValueError("""An invalid option for `--model` was supplied,
                                 options are ['LSTM', 'GRU', 'RNN_TANH' or 'RNN_RELU']""")
 
-        self.embed = nn.Embedding(self.V, edim, padding_idx=None)
-        self.encode = nn.Linear(hdim, zdim)
+        if bidirectional:
+            packed_hdim = 2 * hdim
+        else:
+            packed_hdim = hdim
+
+        self.embed = nn.Embedding(self.V, edim, padding_idx=0)
+        self.encode = nn.Linear(packed_hdim, zdim)
+
         self.init_weights()
+
 
     def init_weights(self):
         initrange = 0.1
         self.embed.weight.data.uniform_(-initrange, initrange)
 
     def forward(self, input, hidden):
-        output, hidden =  self.rnn(self.embed(input), hidden)
-        return self.encode(output), hidden
+        output, hidden = self.rnn(self.embed(input), hidden)
+        output = output[-1, :, :]
+        return self.encode(output)
+
 
 
 class RNNDecoder(nn.Module):
-    def __init__(self, rnn_type, nlayers, vocab, edim, hdim, zdim, bidirectional=True):
+    def __init__(self, batch_size, rnn_type, nlayers, bidirectional, edim, hdim, zdim, vocab):
         super(RNNDecoder, self).__init__()
+
         self.w2i = {w: i for (i, w) in enumerate(vocab)}
         self.i2w = {i: w for (w, i) in self.w2i.items()}
+
         self.V = len(self.w2i)
+        self.batch_size = batch_size
+        self.hdim = hdim
 
         if rnn_type in ['LSTM', 'GRU']:
-            self.rnn = getattr(nn, rnn_type)(self.V, hdim, nlayers, bidirectional=bidirectional)
+            self.rnn = getattr(nn, rnn_type)(edim, hdim, nlayers, bidirectional=bidirectional)
         else:
             raise ValueError("""An invalid option for `--model` was supplied,
                                 options are ['LSTM', 'GRU', 'RNN_TANH' or 'RNN_RELU']""")
 
 
-        self.embed = nn.Embedding(self.V, edim)
-        self.decode = nn.Linear(zdim, hdim)
-        self.tovocab = nn.Linear(hdim, self.V)
-        self.eos = self.embed(['[EOS]'])
+        if bidirectional:
+            packed_hdim = 2 * hdim
+        else:
+            packed_hdim = hdim
+
+        self.embed = nn.Embedding(self.V, edim, padding_idx=0)
+        self.decode = nn.Linear(zdim, packed_hdim)
+        self.tovocab = nn.Linear(packed_hdim, self.V)
+        self.eos = self.embed(torch.tensor(batch_size * [self.w2i['[EOS]']]))
         self.init_weights()
 
     def init_weights(self):
@@ -146,10 +177,14 @@ class RNNDecoder(nn.Module):
 
     def forward(self, z):
         hidden = self.decode(z)
-        seq = [self.w2i['[BOS]']]
+        seq = [self.w2i[BOS]]
         log_p = []
-        while seq[-1] != self.w2i['[EOS]']:
-            output, hidden = self.rnn(self.eos.view(1, 1, -1), hidden)
+        while seq[-1] != self.w2i[EOS]:
+            # print()
+            # print('!!!!', self.eos.view(1, batch_size, -1).shape)
+            # print()
+            output, hidden = self.rnn(self.eos.view(1, self.batch_size, -1),
+                                      hidden.view(-1, self.batch_size, self.hdim))
             log_p_output = F.log_softmax(self.tovocab(output))
             next_token_id = torch.argmax(log_p_output).item()
             log_p.append(log_p_output)
@@ -158,37 +193,36 @@ class RNNDecoder(nn.Module):
 
 
 class VAE(nn.Module):
-    def __init__(self,
-                 rnn_type, nlayers, vocab_size, edim, zdim, hdim, bidirectional=True):
+    def __init__(self, batch_size, rnn_type, nlayers, bidirectional, edim, hdim, zdim, vocab):
         super(VAE, self).__init__()
 
-        self.encoder = RNNEncoder(rnn_type, nlayers, vocab_size, edim, hdim, zdim, bidirectional)
-        self.decoder = RNNDecoder(rnn_type, nlayers, vocab_size, zdim, hdim, bidirectional)
+        self.encoder = RNNEncoder(batch_size, rnn_type, nlayers, bidirectional, edim, hdim, zdim, vocab)
+        self.decoder = RNNDecoder(batch_size, rnn_type, nlayers, bidirectional, edim, hdim, zdim, vocab)
         self.project_loc = nn.Linear(zdim, zdim)
         self.project_scale = nn.Linear(zdim, zdim)
 
+        self.init_hidden = torch.Tensor(torch.zeros(nlayers + int(bidirectional) * nlayers,
+                                                    batch_size,
+                                                    hdim))
+
     def encode(self, input, hidden):
         h = self.encoder(input, hidden)
-        return self.project_loc(h), self.project_scale(h).softplus()
+        return self.project_loc(h), F.softplus(self.project_scale(h))
 
     def reparametrize(self, loc, scale):
         std = torch.exp(0.5 * scale)
         eps = torch.randn_like(std)
-        return loc + eps * std
+        return loc + eps * std  # z
 
     def decode(self, z):
         log_p, seq = self.decoder(z)
         return log_p, seq
 
-    def forward(self, input, hidden):
-        loc, scale = self.encode(input, hidden)
+    def forward(self, input):
+        loc, scale = self.encode(input, self.init_hidden)
         z = self.reparametrize(loc, scale)
         log_p, seq = self.decode(z)
         return log_p, seq, loc, scale
-
-
-model = VAE().to(device)
-optimizer = optim.Adam(model.parameters(), lr=1e-3)
 
 
 # Reconstruction + KL divergence losses summed over all elements and batch
@@ -199,11 +233,13 @@ def loss_function(logp, target, seq, loc, scale, anneal_function=None):
     return nll_loss + kl_loss
 
 
-def train(train_data, valid_data, epoch):
+def train(model, train_split, valid_split, batch_size, epoch):
     model.train()
     train_loss = 0
-    for batch_idx, (data, _) in enumerate(train_data):
+    for batch_idx, data in enumerate(train_split.data(batch_size)):
         data = data.to(device)
+        # print(data.shape)
+        # print(data)
         optimizer.zero_grad()
         log_p, seq, loc, scale = model(data)
         loss = loss_function(log_p, data, seq, loc, scale).item()
@@ -212,19 +248,19 @@ def train(train_data, valid_data, epoch):
         optimizer.step()
         if batch_idx % args.log_interval == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_data.dataset),
-                100. * batch_idx / len(train_data),
+                epoch, batch_idx * len(data), len(train_split),
+                100. * batch_idx / len(train_split),
                 loss.item() / len(data)))
 
     print('====> Epoch: {} Average loss: {:.4f}'.format(
-          epoch, train_loss / len(train_data.dataset)))
+          epoch, train_loss / len(train_split)))
 
 
-def test(test_data, epoch):
+def test(model, test_split, batch_size, epoch):
     model.eval()
     test_loss = 0
     with torch.no_grad():
-        for i, (data, _) in enumerate(test_data):
+        for i, data in enumerate(test_split.data(batch_size)):
             data = data.to(device)
             log_p, seq, loc, scale = model(data)
             test_loss += loss_function(log_p, data, seq, loc, scale).item()
@@ -235,13 +271,34 @@ def test(test_data, epoch):
             #     save_image(comparison.cpu(),
             #              'results/reconstruction_' + str(epoch) + '.png', nrow=n)
 
-    test_loss /= len(test_data)
+    test_loss /= len(test_split)
     print('====> Test set loss: {:.4f}'.format(test_loss))
 
+
 if __name__ == "__main__":
+
+    corpus = Corpus(train_path='data/23.auto.clean',
+                    test_path='data/23.auto.clean',
+                    validation_path='data/22.auto.clean')
+
+    batch_size = 16
+
+    print('!!!  V =', len(corpus.vocab))
+    model = VAE(batch_size,
+                rnn_type='GRU',
+                nlayers=1,
+                bidirectional=True,
+                edim=353,
+                hdim=191,
+                zdim=13,
+                vocab=corpus.vocab)
+    model.to(device)
+    optimizer = optim.Adam(model.parameters(), lr=1e-3)
+
+
     for epoch in range(1, args.epochs + 1):
-        train(epoch)
-        test(epoch)
+        train(model, corpus.training, corpus.validation, batch_size, epoch)
+        test(model, corpus.test, batch_size, epoch)
         # with torch.no_grad():
         #     sample = torch.randn(64, 20).to(device)
         #     sample = model.decode(sample).cpu()
