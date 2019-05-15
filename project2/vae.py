87d4bc3fb936ccccda22c46d4c3aa7cd30c15a2f
mario <m.giulianelli.m@gmail.com>
2019-05-15 15:35:44 +0200

    VAE without importance sampling, dropout, and annealing

--- project2/vae.py
+++ project2/vae.py
@@ -51,18 +51,23 @@ class Split(object):
     def __init__(self, sentences, w2i, n=None):
         self.sentences = sentences
         self.w2i = w2i
+        self.V = len(w2i)
         if n is None:
-            self.len = len(sentences)
+            self.length = len(sentences)
         else:
-            self.len = n
+            self.length = n
 
     def data(self, batch_size):
         for batch in batches(self.sentences, batch_size):
-            batch = [torch.tensor([self.w2i[token] for token in sentence]) for sentence in batch]
-            yield pad_sequence(batch)
+            input_batch = [torch.tensor([self.w2i[token] for token in sentence[:-1]]) for sentence in batch]
+            target_batch = [torch.tensor([self.w2i[token] for token in sentence[1:]]) for sentence in batch]
+            input_batch = pad_sequence(input_batch)
+            target_batch = pad_sequence(target_batch)
+            # onehot = torch.Tensor(torch.zeros(batch.size(0), self.V).scatter_(1, batch, 1.), type=torch.LongTensor)
+            yield input_batch, target_batch
 
     def __len__(self):
-        return self.len
+        return self.length
 
 
 class Corpus(object):
@@ -76,9 +81,9 @@ class Corpus(object):
         self.w2i = {w: i for (i, w) in enumerate(self.vocab)}
         self.i2w = {i: w for (w, i) in self.w2i.items()}
 
-        self.training = Split(train_sentences, self.w2i, len)
-        self.test = Split(test_sentences, self.w2i, len)
-        self.validation = Split(valid_sentences, self.w2i, len)
+        self.training = Split(train_sentences, self.w2i, train_len)
+        self.test = Split(test_sentences, self.w2i, test_len)
+        self.validation = Split(valid_sentences, self.w2i, valid_len)
 
     def load(self, path, counter=None):
         if counter is None:
@@ -91,7 +96,7 @@ class Corpus(object):
                 tree = Tree.fromstring(line)
                 sentence = tree.leaves()
                 counter += Counter(sentence)
-                sentence = sentence + [EOS]
+                sentence = [BOS] + sentence + [EOS]
                 sentences.append(sentence)
                 # if len(sentence) > max_seq_len:
                 #     max_seq_len = len(sentence)
@@ -118,7 +123,7 @@ class RNNEncoder(nn.Module):
             self.rnn = getattr(nn, rnn_type)(edim, hdim, nlayers, bidirectional=bidirectional)
         else:
             raise ValueError("""An invalid option for `--model` was supplied,
-                                options are ['LSTM', 'GRU', 'RNN_TANH' or 'RNN_RELU']""")
+                                options are ['LSTM', 'GRU']""")
 
         if bidirectional:
             packed_hdim = 2 * hdim
@@ -137,8 +142,9 @@ class RNNEncoder(nn.Module):
 
     def forward(self, input, hidden):
         output, hidden = self.rnn(self.embed(input), hidden)
+        len = output.size(0)
         output = output[-1, :, :]
-        return self.encode(output)
+        return self.encode(output), len
 
 
 
@@ -157,7 +163,7 @@ class RNNDecoder(nn.Module):
             self.rnn = getattr(nn, rnn_type)(edim, hdim, nlayers, bidirectional=bidirectional)
         else:
             raise ValueError("""An invalid option for `--model` was supplied,
-                                options are ['LSTM', 'GRU', 'RNN_TANH' or 'RNN_RELU']""")
+                                options are ['LSTM', 'GRU']""")
 
 
         if bidirectional:
@@ -168,28 +174,17 @@ class RNNDecoder(nn.Module):
         self.embed = nn.Embedding(self.V, edim, padding_idx=0)
         self.decode = nn.Linear(zdim, packed_hdim)
         self.tovocab = nn.Linear(packed_hdim, self.V)
-        self.eos = self.embed(torch.tensor(batch_size * [self.w2i['[EOS]']]))
         self.init_weights()
 
     def init_weights(self):
         initrange = 0.1
         self.embed.weight.data.uniform_(-initrange, initrange)
 
-    def forward(self, z):
-        hidden = self.decode(z)
-        seq = [self.w2i[BOS]]
-        log_p = []
-        while seq[-1] != self.w2i[EOS]:
-            # print()
-            # print('!!!!', self.eos.view(1, batch_size, -1).shape)
-            # print()
-            output, hidden = self.rnn(self.eos.view(1, self.batch_size, -1),
-                                      hidden.view(-1, self.batch_size, self.hdim))
-            log_p_output = F.log_softmax(self.tovocab(output))
-            next_token_id = torch.argmax(log_p_output).item()
-            log_p.append(log_p_output)
-            seq.append(next_token_id)
-        return torch.stack(log_p), torch.stack(seq)
+    def forward(self, input, z):
+        h0 = self.decode(z).view(-1, input.size(1), self.hdim)
+        output, _ = self.rnn(self.embed(input), h0)
+        log_p = F.log_softmax(self.tovocab(output), dim=-1)
+        return log_p
 
 
 class VAE(nn.Module):
@@ -201,87 +196,138 @@ class VAE(nn.Module):
         self.project_loc = nn.Linear(zdim, zdim)
         self.project_scale = nn.Linear(zdim, zdim)
 
-        self.init_hidden = torch.Tensor(torch.zeros(nlayers + int(bidirectional) * nlayers,
-                                                    batch_size,
-                                                    hdim))
+        self.nlayers = nlayers
+        self.bidirectional = bidirectional
+        self.hdim = hdim
 
-    def encode(self, input, hidden):
-        h = self.encoder(input, hidden)
-        return self.project_loc(h), F.softplus(self.project_scale(h))
+    def encode(self, input):
+        h0 = torch.Tensor(torch.zeros(self.nlayers + int(self.bidirectional) * self.nlayers,
+                                      input.size(1),
+                                      self.hdim))
+        h, len = self.encoder(input, h0)
+        return self.project_loc(h), F.softplus(self.project_scale(h)), len
 
     def reparametrize(self, loc, scale):
         std = torch.exp(0.5 * scale)
         eps = torch.randn_like(std)
         return loc + eps * std  # z
 
-    def decode(self, z):
-        log_p, seq = self.decoder(z)
-        return log_p, seq
+    def decode(self, input, z):
+        return self.decoder(input, z)
 
     def forward(self, input):
-        loc, scale = self.encode(input, self.init_hidden)
+        loc, scale, len = self.encode(input)
+        # print('loc, scale', loc.shape, scale.shape, len)
         z = self.reparametrize(loc, scale)
-        log_p, seq = self.decode(z)
-        return log_p, seq, loc, scale
+        # print('z', z.shape)
+        log_p = self.decode(input, z)
+        # print('logp', log_p.shape)
+        return log_p.permute(0, 2, 1), loc, scale
 
+
+def word_prediction_accuracy(logp, target):
+    argmax = torch.argmax(logp, dim=1)
+    return torch.mean(torch.eq(argmax, target).float())
 
 # Reconstruction + KL divergence losses summed over all elements and batch
-def loss_function(logp, target, seq, loc, scale, anneal_function=None):
-    NLL = torch.nn.NLLLoss()
+def loss_function(logp, target, loc, scale, anneal_function=None):
+    NLL = torch.nn.NLLLoss(ignore_index=0)
     nll_loss = NLL(logp, target)
-    kl_loss = -0.5 * torch.sum(1 + scale - loc.pow(2) - scale.exp())
+    kl_loss = -0.5 * torch.sum(1 + scale - loc - scale.exp())
     return nll_loss + kl_loss
 
 
-def train(model, train_split, valid_split, batch_size, epoch):
+def train(model, optimizer, train_split, batch_size, epoch):
     model.train()
     train_loss = 0
-    for batch_idx, data in enumerate(train_split.data(batch_size)):
+    wpa = 0
+    n_batches = 0
+    for batch_idx, (data, target) in enumerate(train_split.data(batch_size)):
+        n_batches += 1
         data = data.to(device)
+        target = target.to(device)
         # print(data.shape)
         # print(data)
         optimizer.zero_grad()
-        log_p, seq, loc, scale = model(data)
-        loss = loss_function(log_p, data, seq, loc, scale).item()
+        # print(data.shape)
+        log_p, loc, scale = model(data)
+        # print('loss', log_p.shape, target.shape)
+
+        loss = loss_function(log_p,
+                             target,
+                             loc,
+                             scale)
         loss.backward()
         train_loss += loss.item()
         optimizer.step()
-        if batch_idx % args.log_interval == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_split),
-                100. * batch_idx / len(train_split),
-                loss.item() / len(data)))
+        wpa += word_prediction_accuracy(log_p, target)
+        # if batch_idx % args.log_interval == 0:
+        #     print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+        #         epoch, batch_idx * len(train_split), len(train_split),
+        #         100. * batch_idx / len(train_split),
+        #         loss.item() / len(data)))
 
-    print('====> Epoch: {} Average loss: {:.4f}'.format(
-          epoch, train_loss / len(train_split)))
+    train_loss /= len(train_split)
+    wpa /= n_batches
+    print('====> Epoch: {} Average training loss: {:.4f}  WPA: {:.4f}'.format(epoch, train_loss, wpa))
+    return train_loss, wpa
+
+def validate(model, optimizer, valid_split, batch_size, epoch):
+    model.eval()
+    valid_loss = 0
+    wpa = 0
+    n_batches = 0
+    for batch_idx, (data, target) in enumerate(valid_split.data(batch_size)):
+        n_batches += 1
+        data = data.to(device)
+        target = target.to(device)
+
+        optimizer.zero_grad()
+        log_p, loc, scale = model(data)
+        loss = loss_function(log_p,
+                             target,
+                             loc,
+                             scale)
+        loss.backward()
+        valid_loss += loss.item()
+        optimizer.step()
+        wpa += word_prediction_accuracy(log_p, target)
+
+    valid_loss /= len(valid_split)
+    wpa /= n_batches
+    print('====> Epoch: {} Average validation loss: WPA: {:.4f}'.format(epoch, valid_loss, wpa))
+    return valid_loss, wpa
 
 
-def test(model, test_split, batch_size, epoch):
+def test(model, test_split, batch_size):
     model.eval()
     test_loss = 0
     with torch.no_grad():
-        for i, data in enumerate(test_split.data(batch_size)):
+        wpa = 0
+        n_batches = 0
+        for i, (data, target) in enumerate(test_split.data(batch_size)):
+            n_batches += 1
             data = data.to(device)
-            log_p, seq, loc, scale = model(data)
-            test_loss += loss_function(log_p, data, seq, loc, scale).item()
-            # if i == 0:
-            #     n = min(data.size(0), 8)
-            #     comparison = torch.cat([data[:n],
-            #                           recon_batch.view(args.batch_size, 1, 28, 28)[:n]])
-            #     save_image(comparison.cpu(),
-            #              'results/reconstruction_' + str(epoch) + '.png', nrow=n)
+            target = target.to(device)
+            log_p, loc, scale = model(data)
+            test_loss += loss_function(log_p, target, loc, scale).item()
+            wpa += word_prediction_accuracy(log_p, target)
 
     test_loss /= len(test_split)
-    print('====> Test set loss: {:.4f}'.format(test_loss))
-
+    wpa /= n_batches
+    print('====> Test set loss: {:.4f}WPA: {:.4f}'.format(test_loss, wpa))
+    return test_loss, wpa
 
 if __name__ == "__main__":
 
-    corpus = Corpus(train_path='data/23.auto.clean',
-                    test_path='data/23.auto.clean',
-                    validation_path='data/22.auto.clean')
+    corpus = Corpus(
+        train_path='data/02-21.10way.clean',
+        # train_path='data/23.auto.clean',
+        validation_path='data/22.auto.clean',
+        test_path='data/23.auto.clean'
+    )
 
-    batch_size = 16
+    batch_size = 64
 
     print('!!!  V =', len(corpus.vocab))
     model = VAE(batch_size,
@@ -292,15 +338,32 @@ if __name__ == "__main__":
                 hdim=191,
                 zdim=13,
                 vocab=corpus.vocab)
+
     model.to(device)
     optimizer = optim.Adam(model.parameters(), lr=1e-3)
 
 
     for epoch in range(1, args.epochs + 1):
-        train(model, corpus.training, corpus.validation, batch_size, epoch)
-        test(model, corpus.test, batch_size, epoch)
-        # with torch.no_grad():
-        #     sample = torch.randn(64, 20).to(device)
-        #     sample = model.decode(sample).cpu()
-        #     save_image(sample.view(64, 1, 28, 28),
-        #                'results/sample_' + str(epoch) + '.png')
+        train(model, optimizer, corpus.training, batch_size, epoch)
+        validate(model, optimizer, corpus.validation, batch_size, epoch)
+        # test(model, corpus.test, batch_size)
+
+
+
+##########################################################################################
+# self.eos = self.embed(torch.tensor(batch_size * [self.w2i['[EOS]']]))
+#
+# seq = []
+# log_p = []
+# for i in range(len):
+#     # print()
+#     # print('!!!!', self.eos.view(1, batch_size, -1).shape)
+#     # print()
+#     output, hidden = self.rnn(input,
+#                               hidden)
+#     log_p_output = F.log_softmax(self.tovocab(output))
+#     next_token_id = torch.argmax(log_p_output, dim=-1)
+#     input = self.embed(next_token_id)
+#     log_p.append(log_p_output)
+#     seq.append(next_token_id)
+# return torch.stack(log_p), torch.stack(seq)
