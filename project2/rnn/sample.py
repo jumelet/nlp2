09a4fa48e9bf134e867ebdf34b05c9904527c123
jumelet <jumeletjaap@gmail.com>
2019-05-23 00:58:15 +0200

    Validation perplexity method

---
+++ project2/rnn/sample.py
@@ -0,0 +1,57 @@
+import torch.nn as nn
+import torch
+
+
+def sample(model, vocab, max_len=20, greedy=True, temp=1.0):
+    model.eval()
+
+    hidden = model.init_hidden(1)
+    token = torch.Tensor([vocab.stoi['<bos>']]).long()
+    sen = []
+
+    for i in range(max_len):
+        with torch.no_grad():
+            out, hidden = model(token.view(1, -1), hidden=hidden)
+        probs = nn.functional.softmax(out[0])
+
+        if greedy:
+            token = torch.argmax(probs)
+        else:
+            probs = probs ** (1 / temp)
+            probs /= probs.sum()
+            token = torch.distributions.categorical.Categorical(probs).sample()
+        sen.append(vocab.itos[token])
+        if vocab.itos[token] == '<eos>':
+            break
+
+    print(' '.join(sen))
+
+    model.train()
+
+
+def perplexity(path, model, vocab):
+    with open(path) as f:
+        lines = [['<bos>'] + l.strip().split(' ') + ['<eos>'] for l in f.read().strip().split('\n')]
+
+    model.eval()
+
+    perps = 0.
+
+    for line in lines:
+        sen = torch.LongTensor([vocab.stoi[w] for w in line])
+        with torch.no_grad():
+            out, hidden = model(sen[:-1].view(1, -1),
+                                hidden=model.init_hidden(1))
+
+        all_probs = nn.functional.softmax(out[0], dim=1)
+        probs = torch.index_select(all_probs, 1, sen[1:])
+
+        x = probs.prod()
+
+        perps += torch.exp(-probs.sum() / len(line[:-1]))
+
+    avg_perp = perps / len(lines)
+
+    print(avg_perp)
+
+    model.train()
