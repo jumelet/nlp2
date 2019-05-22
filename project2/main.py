c887bff1433fb573a4a2ab799fdafe6d483943f3
jumelet <jumeletjaap@gmail.com>
2019-05-23 00:57:28 +0200

    Allow model saving and different batching methods

--- project2/main.py
+++ project2/main.py
@@ -6,13 +6,15 @@ train_path = 'project_2_data/02-21.10way.clean'
 if __name__ == '__main__':
     config = {
         'batch_size': 64,
+        'bptt_len': 20,
+        'checkpoint': 'pickles/05-23_00:54/state_dict_e1.pt',
         'device': 'cpu',
-        'epochs': 10,
+        'epochs': 1,
         'learning_rate': 1e-3,
         'hidden_size': 256,
         'input_emb_dim': 256,
         'num_layers': 1,
-        'sample_every': 10,
+        'sample_every': 5,
         'sample_greedy': False,
         'temperature': 0.5,
     }
--- project2/rnn/train.py
+++ project2/rnn/train.py
@@ -1,95 +1,124 @@
+import datetime
+import os
+
 import torch
-import torch.nn as nn
 import torch.optim as optim
 from torch.nn.utils.rnn import pack_padded_sequence
-from torchtext.data import BucketIterator, Field, TabularDataset
+from torchtext.data import BPTTIterator, BucketIterator, Field, TabularDataset
+from torchtext.datasets import PennTreebank
 from tqdm import tqdm
 
 from .rnn import LM
+from .sample import perplexity, sample
 
 
-def sample(model, vocab, max_len=20, greedy=True, temp=1.0):
-    model.eval()
-
-    hidden = model.init_hidden(1)
-    token = torch.Tensor([vocab.stoi['<bos>']]).long()
-    sen = []
-
-    for i in range(max_len):
-        out, hidden = model(token.view(1, -1), hidden=hidden)
-        probs = nn.functional.softmax(out[0])
-
-        if greedy:
-            token = torch.argmax(probs)
-        else:
-            probs = probs ** (1 / temp)
-            probs /= probs.sum()
-            token = torch.distributions.categorical.Categorical(probs).sample()
-        sen.append(vocab.itos[token])
-        if vocab.itos[token] == '<eos>':
-            break
-
-    print(' '.join(sen))
-
-    model.train()
-
-
-def train(config):
+def initialize(config):
     print('Corpus initialization...')
-    field = Field(batch_first=True,
-                  include_lengths=True,
-                  init_token='<bos>',
-                  eos_token='<eos>',
-                  )
-    corpus = TabularDataset(path='project_2_data/train_lines.txt',
-                            format='tsv',
-                            fields=[('text', field)]
-                            )
+    if config.get('bptt_len', None) is not None:
+        field = Field(batch_first=True,
+                      tokenize=lambda s: ['<bos>'] + s.split(' ')
+                      )
+        corpus = PennTreebank('data/train_lines.txt', field)
+    else:
+        field = Field(batch_first=True,
+                      include_lengths=True,
+                      init_token='<bos>',
+                      eos_token='<eos>',
+                      )
+        corpus = TabularDataset(path='data/train_lines.txt',
+                                format='tsv',
+                                fields=[('text', field)]
+                                )
     field.build_vocab(corpus)
 
-    iterator = BucketIterator(dataset=corpus,
-                              batch_size=config['batch_size'],
-                              device=config['device'],
-                              repeat=False,
-                              sort=False,
-                              sort_key=lambda x: len(x.text),
-                              sort_within_batch=True,
-                              )
+    if config.get('bptt_len', None) is not None:
+        iterator = BPTTIterator(corpus,
+                                config['batch_size'],
+                                config['bptt_len'],
+                                device=config['device'],
+                                repeat=False,
+                                )
+    else:
+        iterator = BucketIterator(dataset=corpus,
+                                  batch_size=config['batch_size'],
+                                  device=config['device'],
+                                  repeat=False,
+                                  sort=False,
+                                  sort_key=lambda x: len(x.text),
+                                  sort_within_batch=True,
+                                  )
 
-    vocab_size = len(field.vocab)
+    vocab = field.vocab
     model = LM(
         config['batch_size'],
-        vocab_size,
+        len(vocab),
         device=config['device'],
         hidden_size=config['hidden_size'],
         input_emb_dim=config['input_emb_dim'],
         lstm_num_layers=config['num_layers'],
     )
 
+    return vocab, iterator, model
+
+
+def train(config):
+    vocab, iterator, model = initialize(config)
+
+    model.train()
     criterion = torch.nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=config['learning_rate'])
 
+    if config.get('checkpoint', None) is not None:
+        checkpoint = torch.load(config['checkpoint'])
+        model.load_state_dict(checkpoint['model_state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        epoch = checkpoint['epoch']
+        losses = checkpoint['losses']
+    else:
+        epoch = 0
+        losses = []
+
+    results_dir = config.get('results_dir', str(datetime.datetime.now()).replace(' ', '_')[5:16])
+    os.mkdir(os.path.join('pickles', results_dir))
+    print('Saving results to:', results_dir)
+
     print('Starting training!')
-    for _ in tqdm(range(config['epochs'])):
+    for epoch in tqdm(range(epoch+1, epoch+config['epochs']+1)):
         for i, batch in enumerate(tqdm(iterator)):
             model.reset_hidden()
 
             optimizer.zero_grad()
 
-            text, lengths = batch.text
-            lengths -= 1
+            if config.get('bptt_len', None) is None:
+                text, lengths = batch.text
+                lengths -= 1
 
-            input_ = text[:, :-1]
-            target = pack_padded_sequence(text[:, 1:], lengths=lengths, batch_first=True)[0]
+                input_ = text[:, :-1]
+                target = pack_padded_sequence(text[:, 1:], lengths=lengths, batch_first=True)[0]
 
-            logits = model(input_, lengths=lengths)[0].contiguous()
-            packed_logits = pack_padded_sequence(logits, lengths=lengths, batch_first=True)[0]
+                logits = model(input_, lengths=lengths)[0]
+                logits = pack_padded_sequence(logits, lengths=lengths, batch_first=True)[0]
+            else:
+                text, target = batch.text.t(), batch.target.view(-1)
+                logits = model(text)[0].view(-1, len(vocab))
 
-            loss = criterion(packed_logits, target)
+            loss = criterion(logits, target)
             loss.backward()
+
             optimizer.step()
 
-            if i % config['sample_every'] == 0:
+            losses.append(loss.item())
+            if i > 0 and i % config['sample_every'] == 0:
                 print('\n', i, loss.item())
-                sample(model, field.vocab, greedy=True)
-                sample(model, field.vocab, greedy=False, temp=config['temperature'])
+                sample(model, vocab, greedy=True)
+                sample(model, vocab, greedy=False, temp=config['temperature'])
+                perplexity('data/val_lines.txt', model, vocab)
+
+            break
+
+        torch.save({
+            'epoch': epoch,
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            'losses': losses,
+        }, f'pickles/{results_dir}/state_dict_e{epoch}.pt')
