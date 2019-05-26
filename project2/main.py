89c4a74afb09639dab4ee26b5d0c01775ae86226
Mario Lisa <mariog@test3.irc.sara.nl.irc.sara.nl>
2019-05-26 11:01:34 +0200

    minor

--- project2/main.py
+++ project2/main.py
@@ -3,26 +3,26 @@ from vae.train import initialize, train, test
 
 if __name__ == '__main__':
     config = {
-        'train_path': 'data/23.auto.clean', #'data/02-21.10way.clean',
+        'train_path': 'data/02-21.10way.clean',
         'valid_path': 'data/22.auto.clean',
         'test_path': 'data/23.auto.clean',
         'batch_size': 64,
-        'bptt_len': 10,
+        'bptt_len': 40,
         # 'checkpoint': 'pickles/05-23_00:54/state_dict_e1.pt',
         'rnn_type': 'GRU',
-        'device': 'cpu',
-        'epochs': 2,
+        'device': 'cuda',
+        'epochs': 3,
         'learning_rate': 1e-3,
-        'hidden_dim': 20,
-        'input_emb_dim': 50,
-        'latent_dim': 5,
+        'hidden_dim': 191,
+        'input_emb_dim': 353,
+        'latent_dim': 13,
         'num_layers': 1,
-        'bidir': False,
+        'bidir': True,
         'word_dropout_prob': 0.4,
-        'importance_samples': 5,
+        'importance_samples': 10,
         'seed': 0
     }
 
     model, vocab, train_iterator, valid_iterator, test_iterator = initialize(config)
-    # train(config, model, train_iterator, valid_iterator)
-    test(config, model, test_iterator)
+    train(config, model, train_iterator, valid_iterator, vocab)
+    test(config, model, test_iterator, vocab)
