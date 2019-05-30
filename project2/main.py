a4c8c2a5a3473f9488a270442863d87478985f6d
mario <m.giulianelli.m@gmail.com>
2019-05-30 10:53:19 +0200

    load config dictionary from file

--- project2/main.py
+++ project2/main.py
@@ -1,27 +1,17 @@
-import torch
+import json
 from vae.train import initialize, train, test
+from vae.analysis import sample_sentences, reconstruct_sentence
 
 if __name__ == '__main__':
-    config = {
-        'train_path': 'data/02-21.10way.clean',
-        'valid_path': 'data/22.auto.clean',
-        'test_path': 'data/22.auto.clean',
-        'batch_size': 64,
-        'bptt_len': 40,
-        # 'checkpoint': 'pickles/05-23_00:54/state_dict_e1.pt',
-        'rnn_type': 'GRU',
-        'device': 'cuda',
-        'epochs': 3,
-        'learning_rate': 1e-3,
-        'hidden_dim': 191,
-        'input_emb_dim': 353,
-        'latent_dim': 13,
-        'num_layers': 1,
-        'bidir': True,
-        'word_dropout_prob': 0.4,
-        'importance_samples': 10,
-        'seed': 0
-    }
+
+    # with open('vae/configs/lstm.config', 'w') as f:
+    #     json.dump(config, f, indent=4)
+
+    with open('vae/configs/lstm.config', 'r') as f:
+        config = json.load(f)
+
     model, vocab, train_iterator, valid_iterator, test_iterator = initialize(config)
     train(config, model, train_iterator, valid_iterator, vocab)
     test(config, model, test_iterator, vocab)
+    # print(sample_sentences(config, model, vocab, max_len=20, n=10))
+    # print(reconstruct_sentence('I go to the cinema every Saturday', config, model, vocab))
